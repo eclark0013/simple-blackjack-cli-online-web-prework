b739747f610c26e81b2cff4d681301511af760a5
Eric Clark <eclark0013@gmail.com>
2019-06-08 22:02:45 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -62,6 +62,12 @@ end
 #####################################################
 
 def runner
-  # code runner here
-end
+  welcome
+  total = initial_round
+  until total>21
+    hit(total)
+    display_card_total(total)
+  end
+end_game
+end 
     
